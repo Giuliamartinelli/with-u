2f2432ff0b05dda5edf8724776daf955c5d49e99
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-11-02 17:57:02 +0100

    controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email:"user1@gmail.com", password:"123456", name:"User1", phone_number:"3450847902")
+User.create(email:"user2@gmail.com", password:"123456", name:"User2", phone_number:"3450847902")
+Angel.create(name:"Angel1", user_id:1, phone_number:"3450847902")
+Angel.create(name:"Angel2", user_id:1, phone_number:"3450847902")
+Angel.create(name:"Angel3", user_id:1, phone_number:"3450847902")
+Angel.create(name:"Angel4", user_id:2, phone_number:"3450847902")
+Angel.create(name:"Angel5", user_id:2, phone_number:"3450847902")
+Angel.create(name:"Angel6", user_id:2, phone_number:"3450847902")
