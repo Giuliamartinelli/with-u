217453ed73e6f8fd031ab5043d7f11a2303f4a61
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-11-03 12:11:18 +0100

    implemented twilio methods in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/profile', to: 'pages#profile', as: 'profile'
   get '/fakecall', to: 'pages#fakecall', as: 'fakecall'
   get '/tutorial', to: 'pages#tutorial', as: 'tutorial'
+  get '/map', to: 'pages#map', as: 'map'
   resources :angels, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
