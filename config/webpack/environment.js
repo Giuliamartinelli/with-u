04e445282f7cff3e210fbf02702ab3383f09d46b
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-11-06 15:04:10 +0100

    test-4 pwa

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -18,3 +18,4 @@ const { config, environment, Environment } = require('@rails/webpacker');
 const WebpackerPwa = require('webpacker-pwa');
 new WebpackerPwa(config, environment);
 module.exports = environment;
+environment.loaders.delete('nodeModules')
