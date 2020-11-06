666efd0767ac9847794f61caaa53b9d560fca4f5
Naelle Burger <naelle.burger@gmx.at>
2020-11-06 20:12:31 +0100

    countdown works

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -4,7 +4,19 @@ var downloadTimer = setInterval(function(){
     clearInterval(downloadTimer);
     document.getElementById("countdown").innerHTML = "Starting Fakecall";
   } else {
-    document.getElementById("countdown").innerHTML = "0:0" + timeleft;
+    document.getElementById("countdown").innerHTML = "-0:0" + timeleft;
   }
   timeleft -= 1;
 }, 1000);
+
+// var minutesLabel = document.getElementById("minutes");
+// var secondsLabel = document.getElementById("seconds");
+// var totalSeconds = 0;
+// setInterval(setTime, 1000);
+
+// function setTime() {
+//   ++totalSeconds;
+//   secondsLabel.innerHTML = pad(totalSeconds % 60);
+//   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+// }
+
