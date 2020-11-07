a2f8692f97ebfe74b6838e55dc061dd05c11bffc
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-11-07 11:06:29 +0100

    countdown works and map starts from parma

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,22 +1,19 @@
 var timeleft = 3;
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Starting Fakecall";
-  } else {
-    document.getElementById("countdown").innerHTML = "-0:0" + timeleft;
+
+const timer = () => document.querySelector('#countdown');
+
+const downloadTimer = () => setInterval(function(){
+
+  if (timer()) {
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        timer().innerHTML = "Starting Fakecall";
+    } else {
+      timer().innerHTML = "-0:0" + timeleft;
+    }
   }
   timeleft -= 1;
 }, 1000);
 
-// var minutesLabel = document.getElementById("minutes");
-// var secondsLabel = document.getElementById("seconds");
-// var totalSeconds = 0;
-// setInterval(setTime, 1000);
 
-// function setTime() {
-//   ++totalSeconds;
-//   secondsLabel.innerHTML = pad(totalSeconds % 60);
-//   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-// }
-
+export { downloadTimer };
