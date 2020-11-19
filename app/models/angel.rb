cb1dc223d38940ecd7030a77225ef9e3b4560584
marco checchi <marco.checchi2@gmail.com>
2020-11-19 13:25:14 +0100

    added within form

--- app/models/angel.rb
+++ app/models/angel.rb
@@ -2,4 +2,5 @@ class Angel < ApplicationRecord
   belongs_to :user
 
   validates :name, :phone_number, presence: true
+  validates :phone_number, format: { with: /\A\+\d{6,}z/ }
 end
--- test/system/angels_test.rb
+++ test/system/angels_test.rb
@@ -5,9 +5,11 @@ class AngelsTest < ApplicationSystemTestCase
  test "lets a signed in user create a new product" do
     login_as users(:george)
     visit "/profile"
-    fill_in :placeholder => "Angel name", with: "Angel George"
-    save_and_open_screenshot
-    fill_in "Phone number", with: "+393450847902"
+    within('form') do
+      fill_in :placeholder => 'Angel name', with: "Angel George"
+      save_and_open_screenshot
+      fill_in "Phone number", with: "+393450847902"
+    end
     click_on 'Create Angel'
     save_and_open_screenshot
     # Should be redirected to Home with new product
