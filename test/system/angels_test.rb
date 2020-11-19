1208c7f42f28d8336807638aaeab089d67983874
marco checchi <marco.checchi2@gmail.com>
2020-11-19 12:53:10 +0100

    placeholder identifier

--- test/system/angels_test.rb
+++ test/system/angels_test.rb
@@ -5,7 +5,7 @@ class AngelsTest < ApplicationSystemTestCase
  test "lets a signed in user create a new product" do
     login_as users(:george)
     visit "/profile"
-    fill_in "Angel name", with: "Angel George"
+    fill_in :placeholder => "Angel name", with: "Angel George"
     save_and_open_screenshot
     fill_in "Phone number", with: "+393450847902"
     click_on 'Create Angel'
