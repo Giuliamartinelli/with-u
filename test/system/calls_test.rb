468280bedec344b8c02883745680fc4b5b4e28c2
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-11-20 11:34:31 +0100

    call test implemented

--- test/system/calls_test.rb
+++ test/system/calls_test.rb
@@ -1,15 +1,32 @@
 require "application_system_test_case"
 
 class CallsTest < ApplicationSystemTestCase
-  #test "log in as user and calls angels from profile_path" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  #end
-  #test "sign up as user, add angels, calls from profile_path" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  #end
+  # test "log in as user, add one angel and call angel from fakecall_path" do
+  #   login_as users(:george)
+  #   visit "/profile"
+  #   fill_in "Angel name", with: "Angel Create"
+  #   fill_in "Phone number", with: "+393450847902"
+  #   click_on 'Create Angel'
+  #   assert_equal profile_path, page.current_path
+  #   first('.audio-restart-call').click
+  #   first('.audio-play-call').click
+  #   first('.btn-accept').click
+  #   first('.btn-add-call').click
+  #   save_and_open_screenshot
+  # end
+# test "log in as user, add one angel, send location and call angel from fakecall_path" do
+#     login_as users(:george)
+#     visit "/profile"
+#     fill_in "Angel name", with: "Angel Create"
+#     fill_in "Phone number", with: "+393450847902"
+#     click_on 'Create Angel'
+#     assert_equal profile_path, page.current_path
+#     first('.audio-restart-call').click
+#     first('.audio-play-call').click
+#     first('.btn-accept').click
+#     first('#send-location-change').click
+#     first('.btn-add-call').click
+#     save_and_open_screenshot
+#   end
 end
 #rails test:system
--- test/system/fakecalls_test.rb
+++ test/system/fakecalls_test.rb
@@ -1,11 +1,11 @@
 require "application_system_test_case"
 
 class FakecallsTest < ApplicationSystemTestCase
- #test "log in as user and clicks on accepts phone call" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  #end
+  # test "log in as user and clicks on accepts phone call" do
+  # login_as users(:george)
+
+  #   assert_equal profile_path, page.current_path
+  # end
   #test "log in as user and pauses the fake audio" do
   #   visit users_url
   #
