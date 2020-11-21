4cf6ea1d4b68677b5807ef42dd565cedf50a6e8c
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-11-21 15:16:54 +0100

    angel number and user number are blank spaces free, after update user redirect to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,14 @@ class User < ApplicationRecord
 
   has_one_attached :photo
 
+  before_validation :strip_blanks
+
+def strip_blanks
+self.phone_number = self.phone_number.gsub(/\s+/, "")
+end
+
   validates :name, :phone_number, presence: true
   validates :phone_number, uniqueness: true, format: { with: /\A.{6,}\z/ }
   validates :email, format: { with: /\A\S+@[a-z]{2,}\.[a-z]{2,}\z/ }, uniqueness: true
+
 end
