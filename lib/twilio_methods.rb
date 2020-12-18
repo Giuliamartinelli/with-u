45af7a5f81dc2888dde9f25e2c105ecfc05d68c7
marco checchi <marco.checchi2@gmail.com>
2020-12-18 18:07:15 +0100

    implemented in controller

--- lib/twilio_methods.rb
+++ lib/twilio_methods.rb
@@ -7,22 +7,22 @@ module TwilioMethods
   Account_sid = ENV['ACCOUNT_SID_SECRET']
   Auth_token = ENV['AUTH_TOKEN_SECRET']
 
-  def self.call_angels(angels_numbers, user)
+  def self.call_angels(angels, user)
     response = Twilio::TwiML::VoiceResponse.new
     response.say(message: "Incoming call from #{user.name}, please hold on")
     response.dial(number: user.phone_number)
     @client = Twilio::REST::Client.new(Account_sid, Auth_token)
-    angels_numbers.each do |angel|
+    angels.each do |angel|
       call = @client.calls.create(twiml: response, to: angel.phone_number, from: Call_api, timeout: "17")
     end
   end
 
-  def self.send_location(angels_numbers, latitude, longitude, user)
+  def self.send_location(angels, latitude, longitude, user)
     text_body = "Hello there, #{user.name} sent to you her location because she doesn't feel safe. Make sure to check in on her."
     @client = Twilio::REST::Client.new(Account_sid, Auth_token)
-    angels_numbers.each do |number|
+    angels.each do |angel|
       message = @client.messages.create(body: text_body, from: "whatsapp:#{Whatsapp_api}",
-                persistent_action: ["geo:#{latitude},#{longitude}"], to: "whatsapp:#{number.phone_number}")
+                persistent_action: ["geo:#{latitude},#{longitude}"], to: "whatsapp:#{angel.phone_number}")
     end
   end
 
@@ -35,10 +35,12 @@ module TwilioMethods
     puts verification.status
   end
 
-  def self.send_location_sms(phone_number, latitude, longitude, user)
+  def self.send_location_sms(angels, latitude, longitude, user)
     text_body = "Hi I'm #{user.name} I'm using withU, I'm not safe and i'm here http://maps.google.com/maps?q=loc:#{latitude},#{longitude}"
     @client = Twilio::REST::Client.new(Account_sid, Auth_token)
-    message = @client.messages.create(body: text_body, from: "+12058968810", to: phone_number)
+    angels.each do |angel|
+      message = @client.messages.create(body: text_body, from: Call_api, to: angel.phone_number)
+    end
   end
 
 
